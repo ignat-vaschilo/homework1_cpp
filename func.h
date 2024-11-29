67740f5aff7c57e669cd1e84879cfe70894fe166
ignat <1gnatvaschilo@gmail.com>
2024-11-29 18:33:36 +0300

    add func.h

---
+++ func.h
@@ -0,0 +1,13 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void writeRandomBytes(ofstream &file, int count);
+void writeFile(int x);
+
+#endif // FUNC_H
+
